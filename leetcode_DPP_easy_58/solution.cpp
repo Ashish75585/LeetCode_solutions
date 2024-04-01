cb8ac7c0e29652c922cab2d416c45d035ef5d59d
Ashish75585 <otherside75585@gmail.com>
2024-04-01 20:09:15 +0530

    initial commit

---
+++ leetcode_DPP_easy_58/solution.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) 
+    {
+        int length = 0;
+        int n = s.size();
+        int i=n-1;
+        while(i>=0 && s[i] == ' ')
+            i--;
+        
+        while(i>=0 && s[i] != ' ')
+        {
+            length++;
+            i--;
+        }
+        return length;
+    }
+};
